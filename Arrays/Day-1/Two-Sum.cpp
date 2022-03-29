3e20a612e72edd213b20c57fdfc9daab1c5ca764
Lazarus-10 <ksarim225@gmail.com>
2022-03-29 11:18:52 +0530

    Day1Q1

---
+++ Arrays/Day-1/Two-Sum.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        for(int i=0; i<nums.size(); i++){
+            int secondNumber = target - nums[i];
+            if(mp.find(secondNumber) != mp.end()){
+                return {i, mp[secondNumber]};
+            }
+            mp[nums[i]] = i;
+        }
+        return {-1, -1};
+    }
+};
