d806c52ba1f02d225276e808979c3e59e5f7a3a7
Lazarus-10 <ksarim225@gmail.com>
2022-03-29 11:21:00 +0530

    Day1Q1

---
+++ Arrays/Day-1/buy_Sell_Stock.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int costPrice = prices[0];
+        int netProfit = 0;
+        for(int i=1; i<pricesgit config --list --show-origin.size(); i++){
+            if(prices[i] > costPrice){
+                netProfit = max(netProfit, prices[i]-costPrice);
+            }else{
+                costPrice = prices[i];
+            }
+        }
+        return netProfit;
+    }
+};
