50356e1761d60267aca464072a9053801d9c7435
Lazarus-10 <ksarim225@gmail.com>
2022-03-30 18:32:40 +0530

    Day3Q2

--- Arrays/Day-3/remove_duplicates.cpp
+++ Arrays/Day-3/remove_duplicates.cpp
@@ -5,18 +5,25 @@ using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int i=0, j=1, n = nums.size();
-        while(j < n && nums[j] == nums[i]){
-            j++;
+        // int i=0, j=1, n = nums.size();
+        // while(j < n && nums[j] == nums[i]){
+        //     j++;
+        // }
+        // while(j < n){
+        //     while(j < n-1 && nums[j] == nums[j+1]){
+        //         j++;
+        //     }
+        //     swap(nums[++i], nums[j++]);
+        // }
+        // return i+1;
+        
+        // Best
+        if(nums.size() < 2)return nums.size();
+        int itr = 1;
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i] != nums[i-1])nums[itr++] = nums[i];
         }
-        while(j < n){
-            while(j < n-1 && nums[j] == nums[j+1]){
-                j++;
-            }
-            swap(nums[++i], nums[j++]);
-        }
-        return i+1;
-      
+        return itr;
         // Another Approach
         // int count = 0; 
         // for(int i=1; i<n; i++){
