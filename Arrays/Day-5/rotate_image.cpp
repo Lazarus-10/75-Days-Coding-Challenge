8f6952499ec49723b2640523820dc217c7fcc1f2
Lazarus-10 <ksarim225@gmail.com>
2022-03-31 12:33:56 +0530

    Day5Q2

---
+++ Arrays/Day-5/rotate_image.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/rotate-image/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        //Transpose
+        for(int i=0; i<n; i++){
+            for(int j = i+1; j<n; j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        
+        // mirror
+        int i = 0, j = n-1;
+        while(i < j){
+            for(int k=0; k<n; k++){
+                swap(matrix[k][i], matrix[k][j]);
+            }
+            i++, j--;
+        }
+    }
+};
